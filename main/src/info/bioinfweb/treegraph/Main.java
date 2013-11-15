e33d6026a266ca2ceb58bb5bb893b2cd15fd63a3
BenStoever <stoever@bioinfweb.info>
2013-11-15 12:16:27 +0000

    Library JARs updated. EXE wrapper updated.

--- main/src/info/bioinfweb/treegraph/Main.java
+++ main/src/info/bioinfweb/treegraph/Main.java
@@ -70,7 +70,7 @@ public class Main extends ProgramMainClass {
 	
 	
 	private Main() {
-		super(new ApplicationVersion(2, 0, 47, 271, ApplicationType.BETA));
+		super(new ApplicationVersion(2, 0, 48, 274, ApplicationType.BETA));
 		errorReporter = new SwingErrorReporter(ERROR_URL, getVersion());
 	}
 	
