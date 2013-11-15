746962442888251f363207678a10ccdeecd1c5ff
BenStoever <stoever@bioinfweb.info>
2013-11-15 13:37:17 +0000

    Access to unfinished GenerateBayesTraitsCommandFile action removed from file menu.

--- main/src/info/bioinfweb/treegraph/Main.java
+++ main/src/info/bioinfweb/treegraph/Main.java
@@ -70,7 +70,7 @@ public class Main extends ProgramMainClass {
 	
 	
 	private Main() {
-		super(new ApplicationVersion(2, 0, 48, 274, ApplicationType.BETA));
+		super(new ApplicationVersion(2, 0, 49, 276, ApplicationType.BETA));
 		errorReporter = new SwingErrorReporter(ERROR_URL, getVersion());
 	}
 	
