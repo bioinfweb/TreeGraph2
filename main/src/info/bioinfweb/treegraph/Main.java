6a2eb4e626844c48633fa7ee828f98e3a9e24bf4
SarahWiechers <wiechers@bioinfweb.info>
2015-10-22 07:12:26 +0000

    New version number 2.6.0 set.

--- main/src/info/bioinfweb/treegraph/Main.java
+++ main/src/info/bioinfweb/treegraph/Main.java
@@ -69,7 +69,7 @@ public class Main extends ProgramMainClass {
 	
 	
 	private Main() {
-		super(new ApplicationVersion(2, 5, 0, 529, ApplicationType.BETA));
+		super(new ApplicationVersion(2, 6, 0, 550, ApplicationType.BETA));
 		errorReporter = new SwingErrorReporter(ERROR_URL, getVersion());
 	}
 	
