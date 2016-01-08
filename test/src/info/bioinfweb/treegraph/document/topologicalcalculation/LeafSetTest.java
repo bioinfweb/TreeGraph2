303344bfe8d88551bf9c19c520fa53e8d5de6d70
BenStoever <stoever@bioinfweb.info>
2016-01-08 14:08:02 +0000

    Copyright information in test project updated.

--- test/src/info/bioinfweb/treegraph/document/topologicalcalculation/LeafSetTest.java
+++ test/src/info/bioinfweb/treegraph/document/topologicalcalculation/LeafSetTest.java
@@ -1,6 +1,6 @@
 /*
  * TreeGraph 2 - A feature rich editor for phylogenetic trees
- * Copyright (C) 2007-2015  Ben Stöver, Sarah Wiechers, Kai Müller
+ * Copyright (C) 2015-2016  Ben Stöver, Sarah Wiechers, Kai Müller
  * <http://treegraph.bioinfweb.info/>
  * 
  * This program is free software: you can redistribute it and/or modify
