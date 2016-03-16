c04de41f767be6a914f558f53c902f0f41037e6e
SarahWiechers <wiechers@bioinfweb.info>
2016-03-16 13:04:36 +0000

    Adjusted BayesTraitsReaderTest to recent changes in main project.

--- test/src/info/bioinfweb/treegraph/document/io/ancestralstate/BayesTraitsReaderTest.java
+++ test/src/info/bioinfweb/treegraph/document/io/ancestralstate/BayesTraitsReaderTest.java
@@ -44,18 +44,18 @@ public class BayesTraitsReaderTest {
 			assertEquals("Pan_paniscus", nodes.get("Pan").getLeafNames().get(0));
 //			assertEquals(0.669772491, nodes.get("Root").getProbabilities().get("S(1) - P(0)"), 0.000001);
 //			assertEquals(0.996520048, nodes.get("Pan").getProbabilities().get("S(0) - P(1)"), 0.000001);  //TODO Check with LibreOffice	
-			assertEquals(1, nodes.get("Root").getCharacterMap().get("0").get("0") + nodes.get("Root").getCharacterMap().get("0").get("1"), 0.000000001);	
-			assertEquals(1, nodes.get("Root").getCharacterMap().get("1").get("0") + nodes.get("Root").getCharacterMap().get("1").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("Pan").getCharacterMap().get("0").get("0") + nodes.get("Pan").getCharacterMap().get("0").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("Pan").getCharacterMap().get("1").get("0") + nodes.get("Pan").getCharacterMap().get("1").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("HomoPan").getCharacterMap().get("0").get("0") + nodes.get("HomoPan").getCharacterMap().get("0").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("HomoPan").getCharacterMap().get("1").get("0") + nodes.get("HomoPan").getCharacterMap().get("1").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("GorillaHomoPan").getCharacterMap().get("0").get("0") + nodes.get("GorillaHomoPan").getCharacterMap().get("0").get("1"), 0.000000001);	
-			assertEquals(1, nodes.get("GorillaHomoPan").getCharacterMap().get("1").get("0") + nodes.get("GorillaHomoPan").getCharacterMap().get("1").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("Pongo").getCharacterMap().get("0").get("0") + nodes.get("Pongo").getCharacterMap().get("0").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("Pongo").getCharacterMap().get("1").get("0") + nodes.get("Pongo").getCharacterMap().get("1").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("GreatApes").getCharacterMap().get("0").get("0") + nodes.get("GreatApes").getCharacterMap().get("0").get("1"), 0.000000001);
-			assertEquals(1, nodes.get("GreatApes").getCharacterMap().get("1").get("0") + nodes.get("GreatApes").getCharacterMap().get("1").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("Root").getSiteMap().get("0").get("0") + nodes.get("Root").getSiteMap().get("0").get("1"), 0.000000001);	
+			assertEquals(1, nodes.get("Root").getSiteMap().get("1").get("0") + nodes.get("Root").getSiteMap().get("1").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("Pan").getSiteMap().get("0").get("0") + nodes.get("Pan").getSiteMap().get("0").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("Pan").getSiteMap().get("1").get("0") + nodes.get("Pan").getSiteMap().get("1").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("HomoPan").getSiteMap().get("0").get("0") + nodes.get("HomoPan").getSiteMap().get("0").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("HomoPan").getSiteMap().get("1").get("0") + nodes.get("HomoPan").getSiteMap().get("1").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("GorillaHomoPan").getSiteMap().get("0").get("0") + nodes.get("GorillaHomoPan").getSiteMap().get("0").get("1"), 0.000000001);	
+			assertEquals(1, nodes.get("GorillaHomoPan").getSiteMap().get("1").get("0") + nodes.get("GorillaHomoPan").getSiteMap().get("1").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("Pongo").getSiteMap().get("0").get("0") + nodes.get("Pongo").getSiteMap().get("0").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("Pongo").getSiteMap().get("1").get("0") + nodes.get("Pongo").getSiteMap().get("1").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("GreatApes").getSiteMap().get("0").get("0") + nodes.get("GreatApes").getSiteMap().get("0").get("1"), 0.000000001);
+			assertEquals(1, nodes.get("GreatApes").getSiteMap().get("1").get("0") + nodes.get("GreatApes").getSiteMap().get("1").get("1"), 0.000000001);
 			
 		}
 		catch (IOException e) {
