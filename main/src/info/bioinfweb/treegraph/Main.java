a9c8dfbded02948a2fd002ec6a2698be1af34554
BenStoever <stoever@bioinfweb.info>
2016-07-12 15:24:10 +0000

    Version information updated.

--- main/src/info/bioinfweb/treegraph/Main.java
+++ main/src/info/bioinfweb/treegraph/Main.java
@@ -69,7 +69,7 @@ public class Main extends ProgramMainClass {
 	
 	
 	private Main() {
-		super(new ApplicationVersion(2, 10, 0, 632, ApplicationType.BETA));
+		super(new ApplicationVersion(2, 10, 0, 637, ApplicationType.BETA));
 		errorReporter = new SwingErrorReporter(ERROR_URL, getVersion());
 	}
 	
