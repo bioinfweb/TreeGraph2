7aadf701f4150467cf0e6fd37bc06d76e90c35b4
BenStoever <stoever@bioinfweb.info>
2018-06-21 17:17:22 +0000

    AddSupportValuesEditTest extended.

--- test/src/info/bioinfweb/treegraph/document/undo/file/AddSupportValuesEditTest.java
+++ test/src/info/bioinfweb/treegraph/document/undo/file/AddSupportValuesEditTest.java
@@ -19,6 +19,9 @@
 package info.bioinfweb.treegraph.document.undo.file;
 
 
+import static info.bioinfweb.treegraph.test.TestTools.*;
+
+
 import info.bioinfweb.commons.SystemUtils;
 import info.bioinfweb.treegraph.document.Document;
 import info.bioinfweb.treegraph.document.Tree;
@@ -35,10 +38,6 @@ import java.io.File;
 import org.junit.Test;
 
 
-import static info.bioinfweb.treegraph.test.TestTools.*;
-import static org.junit.Assert.* ;
-
-
 
 public class AddSupportValuesEditTest {
 	private static final String IMPORT_ID = "import";
@@ -48,7 +47,11 @@ public class AddSupportValuesEditTest {
 			new HiddenBranchDataAdapter(IMPORT_ID + AddSupportValuesEdit.CONFLICT_NAME);
 
 	
-	//TODO Testfälle: Source in Target, Target in Source und beides zum jeweils unterschiedlichen zusätlichen und/oder fehlenden Taxa
+	//TODO Add more test cases
+	//     - Test case where only one terminal from the combined set is above a node (inverted case of test_polytomyIntoSolvedAddTerminals())
+	//     - Test conflicting values for all situations
+	//     - Test unrooted cases. Add parameter to executeEdit(). (Support values directly below the root should not be imported then, but currently seem to be for in test_polytomyIntoSolvedAddTerminals(). This needs to be checked.)
+	//     - Test cases with support near root, especially regarding TODO in AddSupportValuesEdit:135.
 	
 	private Tree executeEdit(String targetName, String sourceName) throws Exception {
 	  Document target = ReadWriteFactory.getInstance().getReader(ReadWriteFormat.XTG).read(
@@ -63,6 +66,7 @@ public class AddSupportValuesEditTest {
 		parameters.setTerminalsAdapter(NodeNameAdapter.getSharedInstance());
 		parameters.setSourceSupportColumn(new TextLabelAdapter("support"));
 		parameters.setTargetType(TextIDElementType.HIDDEN_BRANCH_DATA);
+		parameters.setRooted(true);
 		
 		target.executeEdit(new AddSupportValuesEdit(target, parameters));
 		
@@ -100,5 +104,44 @@ public class AddSupportValuesEditTest {
 		
 		assertAnnotation(tree, "yt4x200u57", SUPPORT_COLUMN, 2.0);
 		assertNoAnnotation(tree, "yt4x200u57", CONFLICT_COLUMN);
+	}
+
+
+	@Test
+	public void test_polytomyIntoSolvedAddTerminals() throws Exception {
+		Tree tree = executeEdit("SolvedAddTerminals.xtg", "PolytomyAddTerminals.xtg");
+		
+		assertNoAnnotation(tree, "egkw6pwe2g", SUPPORT_COLUMN);
+		assertNoAnnotation(tree, "egkw6pwe2g", CONFLICT_COLUMN);
+		
+		assertAnnotation(tree, "x7474zcnij", SUPPORT_COLUMN, 2.0);
+		assertNoAnnotation(tree, "x7474zcnij", CONFLICT_COLUMN);
+		
+		assertAnnotation(tree, "yt4x200u57", SUPPORT_COLUMN, 1.0);
+		assertNoAnnotation(tree, "yt4x200u57", CONFLICT_COLUMN);
+		
+		assertNoAnnotation(tree, "051l0uv3ok", SUPPORT_COLUMN);
+		assertNoAnnotation(tree, "051l0uv3ok", CONFLICT_COLUMN);
+		
+		assertNoAnnotation(tree, "71ralxvypp", SUPPORT_COLUMN);
+		assertNoAnnotation(tree, "71ralxvypp", CONFLICT_COLUMN);
+	}
+	
+	
+	@Test
+	public void test_solvedIntoPolytomyAddTerminals() throws Exception {
+		Tree tree = executeEdit("PolytomyAddTerminals.xtg", "SolvedAddTerminals.xtg");
+		
+		assertNoAnnotation(tree, "egkw6pwe2g", SUPPORT_COLUMN);
+		assertNoAnnotation(tree, "egkw6pwe2g", CONFLICT_COLUMN);
+		
+		assertNoAnnotation(tree, "mcvscb0eq5", SUPPORT_COLUMN);
+		assertNoAnnotation(tree, "mcvscb0eq5", CONFLICT_COLUMN);
+		
+		assertAnnotation(tree, "yt4x200u57", SUPPORT_COLUMN, 2.0);
+		assertNoAnnotation(tree, "yt4x200u57", CONFLICT_COLUMN);
+		
+		assertNoAnnotation(tree, "cz8kravy5o", SUPPORT_COLUMN);
+		assertNoAnnotation(tree, "cz8kravy5o", CONFLICT_COLUMN);
 	}
 }
